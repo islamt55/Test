c060feac9f3bc3036706ed84d7493811c90129a4
islam <eslamzoro5@gmail.com>
2019-12-25 01:26:15 +0200

    modified

--- FlameSensorWithBuzzer/FlameSensorWithBuzzer.ino
+++ FlameSensorWithBuzzer/FlameSensorWithBuzzer.ino
@@ -1,9 +1,13 @@
+/*Flame Sensor with Buzzer and Led*/
+
+/*Definig the Arduino Pins*/
 int flame = 3;
 int led = 5;
 int buzzer = 8;
 int Fire ;
 
 void setup() {
+  /*Determine pins'status */
 pinMode(3,INPUT);
 pinMode(5,OUTPUT);
 pinMode(8,OUTPUT);
@@ -11,6 +15,7 @@ pinMode(8,OUTPUT);
 }
 
 void loop() {
+  /*The action taken*/
 Fire = digitalRead(3);
 if (Fire == HIGH){
     digitalWrite(5,HIGH);
