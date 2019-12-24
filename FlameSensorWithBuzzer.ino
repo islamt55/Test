e70b7b1aea59481764e1ed43c4c99fb593e3d92c
Islam <eslamzoro5@gmail.com>
2019-12-25 00:56:40 +0200

    Intial commit

---
+++ FlameSensorWithBuzzer.ino
@@ -0,0 +1,26 @@
+int flame = 3;
+int led = 5;
+//int buzzer = 8;
+int Fire ;
+
+void setup() {
+pinMode(3,INPUT);
+pinMode(5,OUTPUT);
+//pinMode(8,OUTPUT);
+
+}
+
+void loop() {
+Fire = digitalRead(3);
+if (Fire == HIGH){
+    digitalWrite(5,HIGH);
+   // digitalWrite(8,HIGH);
+    delay(1000);
+    digitalWrite(5,LOW);
+    //digitalWrite(8,LOW);
+    delay(1000);
+  
+  }
+  
+
+}
